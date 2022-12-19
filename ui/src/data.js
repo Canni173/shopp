38e7d13ac96d4a8410d7b4522a3d6ea24ea04656
Canni173 <82541731+Canni173@users.noreply.github.com>
2022-12-19 18:48:52 +0100

    Initial commit

---
+++ ui/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://pro2-bar-s3-cdn-cf3.myportfolio.com/c8ff19021ce0d3ba2f42ac71e66d0882/e81dd8b0-9751-427a-8f66-f903becb0885_rwc_0x0x1000x1000x1000.gif?h=8bbc724288b4de3b1dccb8a4e1bd4542",
+      title: "FASHION LIFE",
+      desc: "COLLECT OR ADD YOUR LOOK.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://lh3.googleusercontent.com/KdWj3dPTK1xO8nLYGRpfow4W0cNEPj2H4pwQ1uveJn5mv9TcsV5QvIt9E_DSViXl-WzFAxuLuiZztqXLDq9hSguMybHiUIrNf5WP_g=w394",
+      title: "FASHION LIFE",
+      desc: "COLLECT OR ADD YOUR LOOK.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://i.gifer.com/5aPA.gif",
+      title: "FASHION LIFE",
+      desc: "COLLECT OR ADD YOUR LOOK.",
+      bg: "f5fafd",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://ae01.alicdn.com/kf/H33dc5bc6207f4a4e93401202a848263bp/Mannen-Hip-Hop-Hoodie-Sweatshirt-Japanse-Geisha-Harajuku-Streetwear-Geborduurde-Bloem-Hoodies-Pullover-Katoen-Herfst-Japan.jpg",
+      title: "TOP CLOTHES",
+      cat:"top"
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/originals/8e/06/5e/8e065edd39661847a58af0e498196464.jpg",
+      title: "BOTTOM CLOTHES",
+      cat:"bottom"
+    },
+    {
+      id: 3,
+      img: "https://footwearnews.com/wp-content/uploads/2019/04/billieeilish_feature.jpg",
+      title: "ACCESSORIES",
+      cat:"accessories"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
